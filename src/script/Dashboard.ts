f89f26ee6b8fbb69ff3fa6ff28c17f657e91ed24
Ildiko Gaspar <ildiko@ildiesign.com>
2019-11-10 15:50:27 +0100

    - Dashboard: Event listeners added

--- src/script/Dashboard.ts
+++ src/script/Dashboard.ts
@@ -3,3 +3,35 @@
 import "../style/dashboard.scss";
 
 console.log( "Dasboard" );
+
+
+const optionsBtn                             = document.getElementById( "dashboard-more-btn" );
+const dashboardSidePanel                     = document.getElementById( "dashboard-side-panel" );
+const dashboardSidePanelCloseBtn             = document.getElementById( "side-panel-close-btn" );
+
+const sideMenuBtn                            = document.getElementById( "mini-menu-btn" );
+const sideBarNavPanel                            = document.getElementById( "sidebar-nav" );
+
+
+
+
+
+optionsBtn.addEventListener( "click", () => {
+
+    dashboardSidePanel.classList.add("visible");
+
+});
+
+dashboardSidePanelCloseBtn.addEventListener( "click", () => {
+
+    dashboardSidePanel.classList.remove("visible");
+
+});
+
+
+sideMenuBtn.addEventListener( "click", () => {
+
+    sideBarNavPanel.classList.add("visible");
+
+});
+
